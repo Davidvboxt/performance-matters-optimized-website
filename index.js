cb37d4fa761fe812fbdac8f796f24f2f15bf981d
modous <mohamed1010@live.nl>
2023-05-10 11:33:34 +0200

    de post werkt

--- index.js
+++ index.js
@@ -32,12 +32,6 @@ app.get('/', (request, response) => {
 import bodyParser from 'body-parser';
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define the '/success' route
-app.post('/success', (req, res) => {
-  const { name, seatNumber, date, time } = req.body;
-  res.render('success', { name, seatNumber, date, time });
-});
-
 app.get("/reserveren", (request, response) => {
   fetchJson().then((data) => {
     response.render("reserveren", data);
@@ -52,15 +46,45 @@ app.get("/detail", async (request, response) => {
     const data = await fetch(uniqueUrl)
         .then((response) => response.json())
         .catch((err) => err);
-    response.render("detail", data);  
 
+    response.render("detail", data.vestigingen[0]); 
 });
 
+//Hiermee reserveer ik en stuur ik data naar de API
+app.post('/success', (request, response) => {
+  //De gestuurde data gaat naar de volgende URL:
+  const postUrl = 'https://api.oba.fdnd.nl/api/v1/'
+  const url = `${postUrl}studieplekReserveringen`
+
+  const newDate = new Date(request.body.beginTijd);
+  request.body.beginTijd = newDate;
+
+  postJson(url, request.body).then((data) => {
+    let newStudiePlekReservering = { ... request.body }
+
+    //In de data zit een data.id Hiermee check ik of er een ID aanwezig is voor een studieplek dat nog niet geresereerd is.
+    if (data.data.id) {
+      response.redirect('/success') 
+    } else {
+      //Als het boek al gereserveerd is dan heeft data.data.id geen id meer en krijg je dus een error message.
+      console.log("errorrrs")
+        const errormessage = `${data.message}: Mogelijk komt dit door het id die al bestaat.`;
+        const newData = {
+          error: errormessage,
+          values: newStudiePlekReservering,
+        };
+        //hiermee stuur ik de gebruiker daar de error pagina.
+        response.render("error", newData);
+      }
+      //console.log(JSON.stringify(data.id));
+  })
+})
+
 // Maak een route voor de succes pagina
-app.get('succes', (request, response) => {
-    fetchJson().then((data) => {
-        response.render('succes', data)
-    })
+app.get('/success', (request, response) => {
+  fetchJson().then((data) => {
+      response.render('success', data)
+  })
 })
 
 // Stel het poortnummer in en start express
